e3b5ffc0090f73c4d4ede7d22a6bd57f022c3b9d
Jeff Ripke <jripke74@gmail.com>
2025-03-01 12:05:33 -0800

    add js code"

--- script.js
+++ script.js
@@ -2,6 +2,26 @@ console.log("Hello! I'm your coding fun fact guide!");
 
 const botName = "botMan";
 const botLocation = "earth";
-const favoriteLanguage = "botenese";
+const favoriteLanguage = "JavaScript";
+let codingFact = "const cannot be changed in " + favoriteLanguage;
 
 console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+console.log(codingFact);
+
+codingFact =
+  "Semicolons are put at the end of statements when using " + favoriteLanguage;
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is used to enhance CSS and HTML.";
+console.log(codingFact);
+
+console.log(
+  "It was fun sharing these facts with you. Goodbye! - " +
+    botName +
+    " from " +
+    botLocation +
+    "."
+);
