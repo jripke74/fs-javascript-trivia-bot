313fafaf9b910900659d003f8b9a3f18a38d4b21
Jeff Ripke <jripke74@gmail.com>
2025-03-01 11:44:20 -0800

    add js

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName = "botMan";
+const botLocation = "earth";
+const favoriteLanguage = "botenese";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
